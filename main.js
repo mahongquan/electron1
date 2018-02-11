c562ebfaa6a58c91d4aaaae6d7cae2506e3b8ddd
mahongquan <mahongquan@sina.com>
2018-02-11 18:28:21 +0800

    react browser

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '/react_browser/index.html'),
     protocol: 'file:',
     slashes: true
   }))
